15dfb70371f614dc8997670bb4f95665f6b56822
developer <team-developer@gmail.com>
2020-08-12 15:55:33 +0800

    update vuex support nested modules

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -1,6 +1,8 @@
 import actions from './actions'
 import mutations from './mutations'
 
+const defaultAvatar = "https://avatar.csdn.net/C/9/6/2_lanbery.jpg";
+
 const user = {
   namespaced: true,
   actions,
@@ -9,7 +11,7 @@ const user = {
     token: state => state.token,
     name: state => state.name || "Admin",
     user: state => state.user || "",
-    avatar: state => state.avatar || "https://avatars0.githubusercontent.com/u/41780738",
+    avatar: state => state.avatar || defaultAvatar,
     status: state => state.status || ""
   },
   state: {
